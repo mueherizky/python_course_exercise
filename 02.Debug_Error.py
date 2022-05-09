1a5546f1263376703d129a82670df945a16379f1
mueherizky <102344789+mueherizky@users.noreply.github.com>
2022-05-09 14:16:52 +0700

    make explanation is readable

--- 02.Debug_Error.py
+++ 02.Debug_Error.py
@@ -1,14 +1,26 @@
 #### Before Debug ####
-print(Day 1 - String Manipulation") ---> printing string must used quotes on head and tail sentences. double or single quotes is doesn't matter.
-print("String Concatenation is done with the "+" sign.") ---> if want use different type of quotes, make sure single quotes and double quotes is correct
-  print('e.g. print("Hello " + "world")') ---> identation or tab is not neccessary on this line
-print(("New lines can be created with a backslash and n.") ---> excess of brackets/paranthesis on head string
+print(Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.") 
+  print('e.g. print("Hello " + "world")') 
+print(("New lines can be created with a backslash and n.") 
+
+
 
 #### After Fix Line ####
+
+# printing string must used quotes on head and tail sentences. double or single quotes is doesn't matter.
 print("Day 1 - String Manipulation")
+
+# if want to use different type of quotes, make sure single quotes and double quotes is correct.
 print("String Concatenation is done with the '+' sign.")
+
+# identation or tab is not neccessary on this line
 print('e.g. print("Hello " + "world")')
+
+# excess of brackets/paranthesis on head string
 print("New lines can be created with a backslash and n.")
+
+
 
 #### Testing based on that's above information ^^^ ####
 print("Hello " + "World \n")
