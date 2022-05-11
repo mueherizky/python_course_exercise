b9c52dfcf80426cc182f28bba274b9f7673b20c6
mueherizky <102344789+mueherizky@users.noreply.github.com>
2022-05-11 20:13:26 +0700

    make explanation more readable

--- 10.Odd_Even_Num_Check.py
+++ 10.Odd_Even_Num_Check.py
@@ -8,7 +8,7 @@ number = int(input("Which number do you want to check? "))
 '''
     Modulo or '%' is operator to calculate how many remainder on number.
     Example: 4 % 2 = 0 ---> (this is an even number)
-              3 % 2 = 1 ---> (1 is remainder, it's an odd number)
+             3 % 2 = 1 ---> (1 is remainder, it's an odd number)
 '''
 if number % 2 == 0:
   print("It's an even number!")
