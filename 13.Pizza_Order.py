de5d057eb71e5c5bc3f43809ad208c38ae56515e
mueherizky <102344789+mueherizky@users.noreply.github.com>
2022-05-11 20:11:34 +0700

    day 3 exercise

---
+++ 13.Pizza_Order.py
@@ -0,0 +1,32 @@
+'''
+    We must use uppercase letter for input
+    or the condition statement will not work
+'''
+
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L \n: ")
+add_pepperoni = input("Do you want pepperoni? Y or N \n: ")
+extra_cheese = input("Do you want extra cheese? Y or N \n: ")
+
+
+if size == "S":
+  bill = 15
+  
+elif size == "M":
+  bill = 20
+
+elif size == "L":
+  bill = 25
+
+if add_pepperoni == "Y":
+  if size == "S":
+    bill += 2
+  else:
+    bill += 3
+
+if extra_cheese == "Y":
+  bill += 1
+
+print(f'Your final bill is: ${bill}')
+
+
